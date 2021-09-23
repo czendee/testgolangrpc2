1d7550cab7bab02ea3aa76f45f93272ee33bd1d5
Carlos Zendejas <czendee@gmail.com>
2021-09-23 12:44:02 -0500

    initial commit version 2 grpc

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/friendsofgo/wishlist
+
+go 1.12
+
+require (
+	github.com/golang/protobuf v1.5.2
+	github.com/grpc-ecosystem/go-grpc-middleware v1.1.0
+//	github.com/grpc-ecosystem/grpc-gateway v1.12.1
+        github.com/grpc-ecosystem/grpc-gateway v1.14.8
+	github.com/joho/godotenv v1.3.0
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/oklog/ulid/v2 v2.0.2
+	github.com/pelletier/go-toml v1.4.0 // indirect
+	github.com/spf13/afero v1.2.2 // indirect
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/viper v1.4.0
+	golang.org/x/net v0.0.0-20191207000613-e7e4b65ae663 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
+	golang.org/x/sys v0.0.0-20191206220618-eeba5f6aabab // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
+	google.golang.org/grpc v1.33.2
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+)
---
+++ main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+//	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+//	"github.com/gengo/grpc-gateway/runtime"
+       "github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+        mul "github.com/czendee/testgolangrpc2/genproto/goclient"
+        muls "github.com/czendee/testgolangrpc2/genproto/goserver"
+
+	"strconv"
+	"fmt"	
+	"strings"
+	"time"
+)
+
+/**************************end handler.go ********************/
+
+func Set(w http.ResponseWriter, hs ...Header) {
+	for _, h := range hs {
+		h.Populate(w.Header())
+	}
+}
+
+type Header interface {
+	Populate(http.Header)
+}
+
+type AccessControl struct {
+	MaxAge           time.Duration
+	Origin           string
+	ExposedHeaders   []string
+	AllowCredentials bool
+	AllowedMethods   []string
+	AllowedHeaders   []string
+}
+
+func (ac AccessControl) Populate(h http.Header) {
+	if ac.Origin != "" {
+		h.Set("Access-Control-Allow-Origin", ac.Origin)
+	}
+	if len(ac.AllowedMethods) > 0 {
+		h.Set("Access-Control-Allow-Methods", strings.Join(ac.AllowedMethods, ", "))
+	}
+}
+
+type ContentType string
+
+func (ct ContentType) Populate(h http.Header) {
+	h.Set("Content-Type", string(ct))
+}
+/**************************end handler.go ********************/
+type server struct{}
+
+
+// SayMultiplica implements multiplica.GreeterServer
+func (s *server) SayMultiplica(ctx context.Context, in *mul.MultiplicaRequest) (*mul.MultiplicaReply, error) {
+	
+	
+	elemento1 :=in.Numero
+	var s1final float64 = 0
+	
+	if s1, err := strconv.ParseFloat(elemento1, 64); err == nil {
+             fmt.Println(s1) // 3.1415927410125732
+		s1final =s1;
+       }
+	
+	elemento2 :=in.Veces
+	var s2final float64 = 0
+	
+	if s2, err := strconv.ParseFloat(elemento2, 64); err == nil {
+             fmt.Println(s2) // 3.1415927410125732
+		s2final =s2;
+       }
+	
+	resultado := s1final* s2final;
+	 fmt.Println(resultado) 
+	sresultado := fmt.Sprintf("%f", resultado)
+
+//	return &mul.MultiplicaReply{Message: " " + in.Numero +" multiplica" + sresultado}, nil
+        return &mul.MultiplicaReply{Message: " " + sresultado}, nil
+}
+
+
+func startGRPC(port string) error {
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		return err
+	}
+	s := grpc.NewServer()
+	
+	mul.RegisterGreeterServer(s, &server{})
+	return s.Serve(lis)
+}
+
+func cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		Set(w, AccessControl{
+			Origin:         "*",
+			AllowedMethods: []string{"GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE", "PATCH"},
+		})
+		next.ServeHTTP(w, r)
+	})
+}
+
+func startHTTP(httpPort, grpcPort string) error {
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+
+
+	gwmuxmultiplica := runtime.NewServeMux()
+	optsmul := []grpc.DialOption{grpc.WithInsecure()}
+	if err := muls.RegisterGreeterHandlerFromEndpoint(ctx, gwmuxmultiplica, "127.0.0.1:"+grpcPort, optsmul); err != nil {
+            
+		return err
+	}
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/v2/", gwmuxmultiplica)
+
+	http.ListenAndServe(":"+httpPort, cors(mux))
+	return nil
+}
+
+func main() {
+	errors := make(chan error)
+
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+
+	grpcPort := os.Getenv("GRPC_PORT")
+	if grpcPort == "" {
+		grpcPort = "50080"
+	}
+
+	if grpcPort == httpPort {
+		panic("Can't listen on the same port")
+	}
+
+	go func() {
+		errors <- startGRPC(grpcPort)
+	}()
+
+	go func() {
+		errors <- startHTTP(httpPort, grpcPort)
+	}()
+
+	for err := range errors {
+		log.Fatal(err)
+		return
+	}
+}
